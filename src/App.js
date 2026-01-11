e25d5aa5feecf678d882fca17ea97086b11988ea
sukru <57896923+kosuke14@users.noreply.github.com>
2026-01-11 17:07:54 +0900

    Fix JSX syntax in App component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import MidiUpload from './MidiUpload.js';
 import Typography from '@mui/material/Typography';
 
 function App() {
-  return (
+  return (<>
     <MidiUpload />
     <Typography><a href="https://github.com/kosuke14/midishare">GitHub</a></Typography>
-  );
+  </>);
 }
 
 export default App;
