fe1f74bd003af4975bfcf532076f67a80500755f
sukru <57896923+kosuke14@users.noreply.github.com>
2026-01-11 17:05:43 +0900

    Add Typography component with GitHub link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MidiUpload from './MidiUpload.js';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <MidiUpload />
+    <Typography><a href="https://github.com/kosuke14/midishare">GitHub</a></Typography>
   );
 }
 
